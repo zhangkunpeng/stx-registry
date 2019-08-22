09170cccfb8cfebcd1632b6256b3d05cf2603014
zhangkunpeng <zhang.kunpeng@99cloud.net>
2019-08-22 09:57:32 +0800

    bug fix

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 set -x
 cp -r src /usr/share/stx-images-update
 cat /etc/crontab |grep "ansible-playbook /usr/share/stx-images-update/update.yml"
-if [ $? -eq 0 ];then
+if [ $? -ne 0 ];then
     echo "0 17 * * * root ansible-playbook /usr/share/stx-images-update/update.yml" >> /etc/crontab
 fi
