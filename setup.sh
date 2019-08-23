0dff112b92048e62b73b3288acf62063c4e92d1b
zhangkunpeng <zhang.kunpeng@99cloud.net>
2019-08-23 12:03:38 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 set -x
+rm -rf /usr/share/stx-images-update
 cp -r src /usr/share/stx-images-update
 cat /etc/crontab |grep "ansible-playbook /usr/share/stx-images-update/update.yml"
 if [ $? -ne 0 ];then
