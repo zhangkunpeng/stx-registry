36a7d23a9135970c673d47e5f9235caf6a448a15
zhangkunpeng <zhang.kunpeng@99cloud.net>
2019-09-26 17:37:47 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ set -x
 rm -rf /usr/share/stx-images-update
 cp -r src /usr/share/stx-images-update
 chmod +x /usr/share/stx-images-update/run.sh
-ln -s /usr/share/stx-images-update/run.sh /etc/cron.daily/stx_update_images.sh
+#ln -s /usr/share/stx-images-update/run.sh /etc/cron.daily/stx_update_images.sh
